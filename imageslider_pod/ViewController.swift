e32f0bb5d0174ee511bf4cafaad8d6126cd8cc6c
pratik2256 <41819896+pratik2256@users.noreply.github.com>
2018-08-10 12:34:17 +0530

    Initial commit

---
+++ imageslider_pod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageslider_pod
+//
+//  Created by TOPS on 8/10/18.
+//  Copyright © 2018 TOPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
